217ded199e199bb938eb65fbc9c80abbb66aedd1
ylwester <sylwester.miecznikowski@gmail.com>
2023-02-12 00:24:13 +0100

    feat(auth): add basic auth error handling (#4)

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -3,9 +3,21 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   onAuthStateChanged,
+  AuthErrorCodes,
 } from 'firebase/auth'
 import { auth } from '../../firebase'
 import type { User as FirebaseUser } from 'firebase/auth'
+import { Alert } from 'react-native'
+
+type AuthErrorsTypes = Partial<
+  Record<(typeof AuthErrorCodes)[keyof typeof AuthErrorCodes], string>
+>
+
+const authErrors = {
+  'auth/too-many-requests':
+    'You made too many requests, please try again later.',
+  'auth/email-already-in-use': 'Email already in use.',
+} satisfies AuthErrorsTypes
 
 export type SignUpProps = {
   email: string
@@ -25,14 +37,24 @@ const useAuth = () => {
   }, [])
 
   const handleSignIn = useCallback(async ({ email, password }: SignUpProps) => {
-    await signInWithEmailAndPassword(auth, email, password)
+    try {
+      await signInWithEmailAndPassword(auth, email, password)
+    } catch (error: any) {
+      if (error.code in authErrors) {
+        // @ts-ignore
+        Alert.alert('Error', authErrors[error.code])
+      } else {
+        Alert.alert('Error', 'Incorrect email or password.')
+      }
+    }
   }, [])
 
   const handleSignUp = useCallback(async ({ email, password }: SignUpProps) => {
     try {
       await createUserWithEmailAndPassword(auth, email, password)
-    } catch (error) {
-      console.error(error)
+    } catch (error: any) {
+      // @ts-ignore
+      Alert.alert('Error', authErrors[error.code])
     }
   }, [])
 
